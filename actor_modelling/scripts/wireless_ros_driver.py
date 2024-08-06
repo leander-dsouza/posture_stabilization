d0d68ec2ffbd9996d00da1895564ea3c3472ef82
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2024-08-06 05:01:27 +0100

    wireless imu message complete

--- actor_modelling/scripts/wireless_ros_driver.py
+++ actor_modelling/scripts/wireless_ros_driver.py
@@ -11,10 +11,14 @@ from tf.transformations import quaternion_from_euler
 from geometry_msgs.msg import Quaternion
 
 
+# Constants
+MOVELLA_BT_DOT_UUID = "D4:22:CD:00:A7:18"
+MEASUREMENT_UUID = '15172001-4947-11e9-8646-d663bd873d93'
+MEDIUM_PAYLOAD_UUID = "15172003-4947-11E9-8646-D663BD873D93"
 
-measurement_characteristic_uuid = '15172001-4947-11e9-8646-d663bd873d93'
-short_payload_characteristic_uuid = "15172004-4947-11e9-8646-d663bd873d93"
-medium_payload_characteristic_uuid = "15172003-4947-11E9-8646-D663BD873D93"
+
+CUSTOM_MODE_1 = bytes([1, 1, 22]) # [Timestamp, Euler, Free Acceleration, Angular Velocity]
+RATE_WITH_MAG = bytes([1, 1, 20]) # [Timestamp, Acceleration, Angular Velocity, Magnetic Field]
 
 
 class WirelessXSensDriver():
@@ -34,67 +38,68 @@ class WirelessXSensDriver():
 
     def notification_callback(self, _, data):
         """Callback function for the notification."""
-        print(self.encode_data(data))
+        encoded_data = self.encode_data(data)
+        # print(encoded_data)
 
         # Publish IMU Data
         imu_msg = Imu()
         imu_msg.header.stamp = rospy.Time.now()
         imu_msg.header.frame_id = "base_link"
 
-        # Set the orientation
-        orientation = self.encode_data(data)
-        q = quaternion_from_euler(orientation['x'], orientation['y'], orientation['z'])
-        imu_msg.orientation = Quaternion(*q)
+        # Set Orientation
+        euler = encoded_data['euler'][0]
+        quaternion = quaternion_from_euler(euler[0], euler[1], euler[2])
+        imu_msg.orientation = Quaternion(*quaternion)
+        imu_msg.orientation_covariance = [1e-9, 0, 0, 0, 1e-9, 0, 0, 0, 1e-9]
+
+        # Set Angular Velocity
+        angular_velocity = encoded_data['angular_velocity'][0]
+        imu_msg.angular_velocity.x = angular_velocity[0]
+        imu_msg.angular_velocity.y = angular_velocity[1]
+        imu_msg.angular_velocity.z = angular_velocity[2]
+        imu_msg.angular_velocity_covariance = [1e-9, 0, 0, 0, 1e-9, 0, 0, 0, 1e-9]
+
+        # Set Linear Acceleration
+        free_acceleration = encoded_data['free_acceleration'][0]
+        imu_msg.linear_acceleration.x = free_acceleration[0]
+        imu_msg.linear_acceleration.y = free_acceleration[1]
+        imu_msg.linear_acceleration.z = free_acceleration[2]
+        imu_msg.linear_acceleration_covariance = [1e-9, 0, 0, 0, 1e-9, 0, 0, 0, 1e-9]
 
         self.imu_pub.publish(imu_msg)
 
 
     def encode_data(self, bytes_):
         """Encode the data."""
-        # data_segments = np.dtype([
-        #     ('timestamp', np.uint32),
-        #     ('roll', np.float32),
-        #     ('pitch', np.float32),
-        #     ('yaw', np.float32),
-        #     ('freeAccX', np.float32),
-        #     ('freeAccY', np.float32),
-        #     ('freeAccZ', np.float32),
-        #     ('gyroX', np.float32),
-        #     ('gyroY', np.float32),
-        #     ('gyroZ', np.float32),
-        #     ('zero_padding', np.uint32)
-        #     ])
-
-        data_segments = np.dtype([
-            ('timestamp', np.uint32),
-            ('x', np.float32),
-            ('y', np.float32),
-            ('z', np.float32),
-            ('zero_padding', np.uint32)
+        custom_mode_1_segments = np.dtype([
+            ('timestamp', np.uint32, 1),
+            ('euler', np.float32, 3),
+            ('free_acceleration', np.float32, 3), # earth's gravity is deducted
+            ('angular_velocity', np.float32, 3),
             ])
-        formatted_data = np.frombuffer(bytes_, dtype=data_segments)
+
+        rate_with_mag_segments = np.dtype([
+            ('timestamp', np.uint32, 1),
+            ('acceleration', np.float32, 3),
+            ('angular_velocity', np.float32, 3),
+            ('magnetic_field', np.float32, 3),
+            ])
+
+        formatted_data = np.frombuffer(bytes_, dtype=custom_mode_1_segments)
         return formatted_data
 
     async def bt_listen(self):
         """Listen to the Bluetooth."""
-        address = "D4:22:CD:00:A7:18" # Movella DOT UUID
-
-        async with BleakClient(address) as client:
+        async with BleakClient(MOVELLA_BT_DOT_UUID) as client:
             # Check if connection was successful
             print(f"Client connection: {client.is_connected}") # prints True or False
 
-            # Subscribe to notifications from the Short Payload Characteristic
-            await client.start_notify(short_payload_characteristic_uuid,
-                                      self.notification_callback)
-
             # Subscribe to notifications from the Medium Payload Characteristic
-            # await client.start_notify(medium_payload_characteristic_uuid,
-            #                           self.notification_callback)
+            await client.start_notify(MEDIUM_PAYLOAD_UUID,
+                                        self.notification_callback)
 
-            # Set and turn on the Custom mode 1 - Timestamp, Euler, Free Acc, Free Angular Vel
-            # binary_message = b"\x01\x01\x22"
-            binary_message = b"\x01\x01\x04"
-            await client.write_gatt_char(measurement_characteristic_uuid,
+            binary_message = CUSTOM_MODE_1
+            await client.write_gatt_char(MEASUREMENT_UUID,
                                          binary_message, response=True)
 
             # Wait for the user to press Ctrl+C
